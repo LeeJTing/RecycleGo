a1d75e50c301fb9cc29d8f67708fdea2d37bfbdd
LeeJTing <168332482+LeeJTing@users.noreply.github.com>
2026-03-21 20:46:07 +0800

    update routes

---
+++ lib/app/routes.dart
@@ -0,0 +1,12 @@
+class Routes {
+  static const String login = '/login';
+  static const String register = '/register';
+
+  static const String adminProfile = '/admin/profile';
+  static const String userProfile = '/user/profile';
+
+  static const String adminDashboard = '/admin/dashboard';
+  static const String userHomePage = '/user/homePage';
+
+
+}
